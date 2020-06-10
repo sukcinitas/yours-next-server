e215fd717f898973ba023b1a2bfb0d46c64738f4
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2020-06-10 15:08:47 +0300

    Add socket functions

--- src/server.js
+++ src/server.js
@@ -4,17 +4,31 @@ const app = require('./app');
 require('dotenv').config();
 
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri,
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const { connection } = mongoose;
 connection.once('open', () => {
-  // eslint-disable-next-line no-console
   console.log('Connection with MongoDB database established!');
 });
 
+io.on('connection', (socket) => {
+  let group;
+  socket.on('authenticate', (data) => { // when user authenticates he joins the group's room
+    group = data.name;
+    socket.join(group);
+    io.sockets.in(group).emit('joinmessage', { message: 'Successfully joined room named ' + group });
+  });
+  socket.on('sendMessage', (data) => {
+    io.sockets.in(group).emit('sendMessage', data);
+  });
+  socket.on('addMember', (data) => {
+    io.sockets.in(group).emit('addMember', data);
+  });
+});
+
 server.listen(process.env.PORT || 8081, () => {
-  // eslint-disable-next-line no-console
   console.log('Server is running on port 8081!');
 });
