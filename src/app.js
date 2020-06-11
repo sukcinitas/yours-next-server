b87d63cd4ab67edc6464c040f4342ac44d9ab7eb
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2020-06-11 15:40:23 +0300

    Configure for production serving

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static('client/dist'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', '*');
@@ -14,14 +16,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page!');
-});
-
 app.use('/api', require('./routes'));
 
 app.all('*', (req, res) => {
-  res.status(404).send('Not found!');
+  res.sendFile(path.join(process.cwd(), '/client/dist/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 module.exports = app;
