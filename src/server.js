51bcdbc3eff6bc4fddc5ecbfdba8eb00a3fbee18
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2020-10-20 20:29:50 +0300

    Wrap socketio

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const http = require('http');
 const socketIO = require('socket.io');
 const app = require('./app');
+const useSocket = require('./socket');
 require('dotenv').config();
 
 const server = http.createServer(app);
@@ -16,99 +17,7 @@ connection.once('open', () => {
   console.log('Connection with MongoDB database established!');
 });
 
-let state = {};
-io.on('connection', (socket) => {
-  let client;
-  let group;
-  socket.on('getInitialState', (data) => { // when user authenticates/creates he joins the group's room
-    group = data.name;
-    socket.join(group);
-    if (Object.keys(state).indexOf(group) !== -1) {
-      socket.emit('setInitialState', { group: 
-        { 
-          activeMembers: state[group].activeMembers,
-          messages: state[group].messages, 
-          moderator: state[group].moderator,
-          ongoingPlaylist: state[group].ongoingPlaylist,
-        }});
-    } else {
-      state[group] = {};
-      state[group].activeMembers = [];
-      state[group].messages = [];
-      state[group].moderator = '',
-      state[group].ongoingPlaylist = {
-        id: '',
-        videoIndex: 0,
-        time: 0, 
-      }
-    }
-  });
-  socket.on('addMember', (data) => {
-    if (state[group].activeMembers.length === 0) {
-      state[group].moderator = data.name;
-      socket.emit('setModerator', data);
-    }
-    state[group].activeMembers.push(data);
-    io.sockets.in(group).emit('addMember', data);
-  });
-  // I only set member in sender
-  socket.on('setMember', (data) => {
-    client = data;
-  });
-  socket.on('sendMessage', (data) => {
-    io.sockets.in(group).emit('sendMessage', data);
-    if (state[group]) { // fallback so it wouldn't crash
-      state[group].messages = [...state[group].messages, {message: data.message, name: data.member }];
-    }
-  });
-  socket.on('updatePlaylists', (data) => {
-    io.sockets.in(group).emit('updatePlaylists', { playlists: data.playlists });
-  });
-  socket.on('updatePlaylist', (data) => {
-    io.sockets.in(group).emit('updatePlaylist', { 
-      idsArray: data.idsArray, 
-      itemData: data.itemData,
-      type: data.type, 
-      alreadyIn: data.alreadyIn,
-      id: data.id,
-    });
-  });
-  socket.on('userJoinsOngoingPlaylist', () => {
-    io.sockets.in(group).emit('userJoinsOngoingPlaylist');  
-  });
-  socket.on('setOngoingPlaylist', (data) => {
-    state[group].ongoingPlaylist.id = data.id;
-    state[group].ongoingPlaylist.videoIndex = data.videoIndex;
-    state[group].ongoingPlaylist.time = data.time;
-    state[group].ongoingPlaylist.paused = false;
-    io.sockets.in(group).emit('setOngoingPlaylist', state[group].ongoingPlaylist);
-  });
-  socket.on('changeOngoingPlaylistVideoIndex', (data) => {
-    state[group].ongoingPlaylist.videoIndex = data.videoIndex;
-    io.sockets.in(group).emit('changeOngoingPlaylistVideoIndex', { videoIndex: data.videoIndex });
-  });
-  socket.on('toggleOngoingPlaylist', (data) => {
-    io.sockets.in(group).emit('toggleOngoingPlaylist', { paused: data.paused });
-  });
-  socket.on('disconnect', () => {
-    if (!state[group] || !client) {
-      return;
-    }
-    if (state[group].activeMembers.length === 1 ) { // last one to disconnect
-      group ? delete state[group] : ''; 
-      return;
-    }
-    state[group].activeMembers = state[group].activeMembers.filter(member => member.name !== client.name );
-    io.sockets.in(group).emit('removeMember', { client: client.name, emoji: client.emoji });
-    if (client.name === state[group].moderator && state[group].activeMembers.length !== 0) {
-      io.sockets.in(group).emit('setModerator', { name: state[group].activeMembers[0].name }); 
-      state[group].moderator = state[group].activeMembers[0].name;
-    }
-  });
-  socket.on('setModerator', (name) => {
-    io.sockets.in(group).emit('setModerator', { name }); 
-  });
-});
+useSocket(io);
 
 const port = process.env.PORT || 8081;
 server.listen(port, () => {
---
+++ src/socket.js
@@ -0,0 +1,94 @@
+let state = {};
+module.exports = (io) =>
+io.on('connection', (socket) => {
+    let client;
+    let group;
+    socket.on('getInitialState', (data) => { // when user authenticates/creates he joins the group's room
+      group = data.name;
+      socket.join(group);
+      if (Object.keys(state).indexOf(group) !== -1) {
+        socket.emit('setInitialState', { group: 
+          { 
+            activeMembers: state[group].activeMembers,
+            messages: state[group].messages, 
+            moderator: state[group].moderator,
+            ongoingPlaylist: state[group].ongoingPlaylist,
+          }});
+      } else {
+        state[group] = {};
+        state[group].activeMembers = [];
+        state[group].messages = [];
+        state[group].moderator = '',
+        state[group].ongoingPlaylist = {
+          id: '',
+          videoIndex: 0,
+          time: 0, 
+        }
+      }
+    });
+    socket.on('addMember', (data) => {
+      if (state[group].activeMembers.length === 0) {
+        state[group].moderator = data.name;
+        socket.emit('setModerator', data);
+      }
+      state[group].activeMembers.push(data);
+      io.sockets.in(group).emit('addMember', data);
+    });
+    // I only set member in sender
+    socket.on('setMember', (data) => {
+      client = data;
+    });
+    socket.on('sendMessage', (data) => {
+      io.sockets.in(group).emit('sendMessage', data);
+      if (state[group]) { // fallback so it wouldn't crash
+        state[group].messages = [...state[group].messages, {message: data.message, name: data.member }];
+      }
+    });
+    socket.on('updatePlaylists', (data) => {
+      io.sockets.in(group).emit('updatePlaylists', { playlists: data.playlists });
+    });
+    socket.on('updatePlaylist', (data) => {
+      io.sockets.in(group).emit('updatePlaylist', { 
+        idsArray: data.idsArray, 
+        itemData: data.itemData,
+        type: data.type, 
+        alreadyIn: data.alreadyIn,
+        id: data.id,
+      });
+    });
+    socket.on('userJoinsOngoingPlaylist', () => {
+      io.sockets.in(group).emit('userJoinsOngoingPlaylist');  
+    });
+    socket.on('setOngoingPlaylist', (data) => {
+      state[group].ongoingPlaylist.id = data.id;
+      state[group].ongoingPlaylist.videoIndex = data.videoIndex;
+      state[group].ongoingPlaylist.time = data.time;
+      state[group].ongoingPlaylist.paused = false;
+      io.sockets.in(group).emit('setOngoingPlaylist', state[group].ongoingPlaylist);
+    });
+    socket.on('changeOngoingPlaylistVideoIndex', (data) => {
+      state[group].ongoingPlaylist.videoIndex = data.videoIndex;
+      io.sockets.in(group).emit('changeOngoingPlaylistVideoIndex', { videoIndex: data.videoIndex });
+    });
+    socket.on('toggleOngoingPlaylist', (data) => {
+      io.sockets.in(group).emit('toggleOngoingPlaylist', { paused: data.paused });
+    });
+    socket.on('disconnect', () => {
+      if (!state[group] || !client) {
+        return;
+      }
+      if (state[group].activeMembers.length === 1 ) { // last one to disconnect
+        group ? delete state[group] : ''; 
+        return;
+      }
+      state[group].activeMembers = state[group].activeMembers.filter(member => member.name !== client.name );
+      io.sockets.in(group).emit('removeMember', { client: client.name, emoji: client.emoji });
+      if (client.name === state[group].moderator && state[group].activeMembers.length !== 0) {
+        io.sockets.in(group).emit('setModerator', { name: state[group].activeMembers[0].name }); 
+        state[group].moderator = state[group].activeMembers[0].name;
+      }
+    });
+    socket.on('setModerator', (name) => {
+      io.sockets.in(group).emit('setModerator', { name }); 
+    });
+});
