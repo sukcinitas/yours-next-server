304d6913b6c8bff2d62c378348e8050219214027
sukcinitas <gintare.cerniauskaite@protonmail.com>
2021-06-04 22:52:02 +0300

    Update submodule

--- src/app.js
+++ src/app.js
@@ -43,6 +43,7 @@ app.use(passport.session());
 app.use(cors());
 
 app.use(express.static('client/dist'));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', '*');
